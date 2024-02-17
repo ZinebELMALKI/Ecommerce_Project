63bdebfe8ff3afee910e57048379792a60adfaf2
ZinebELMALKI <elmalki.z.fst@uhp.ac.ma>
2024-02-17 21:58:46 +0100

    message

--- store/urls.py
+++ store/urls.py
@@ -10,4 +10,7 @@ from django.conf import settings
 urlpatterns = [
 
     path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/',
+         views.product_detail, name='product_detail'),
 ]
