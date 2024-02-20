18307e1133c6fc4558b8c92ddf2ec1c89157a694
ZinebELMALKI <elmalki.z.fst@uhp.ac.ma>
2024-02-20 16:07:56 +0100

    message

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import sessions
 from store.models import Product
 from .models import Cart, CartItem
@@ -35,9 +35,27 @@ def add_cart(request, product_id):
             cart=cart,
         )
     cart_item.save()
-    return HttpResponse(cart_item.quantity)
-    exit()
+    
     return redirect('cart')
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product = product, cart=cart)
+    if cart_item.quantity > 1 :
+        cart_item.quantity -= 1
+        cart_item.save()
+    else :
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product = product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
@@ -47,6 +65,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
     except ObjectNotExist:
         pass
 
@@ -54,5 +74,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
